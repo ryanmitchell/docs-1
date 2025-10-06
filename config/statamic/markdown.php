86505d8d3e313f1d87870aa0988d4754bac159b9
Duncan McClean <duncan@duncanmcclean.com>
2025-10-06 12:11:38 +0100

    Add back anchor link on hover to headings
    Closes #1736

--- config/statamic/markdown.php
+++ config/statamic/markdown.php
@@ -19,12 +19,13 @@ return [
 
         'default' => [
              'heading_permalink' => [
-                 'symbol' => '',
                  'id_prefix' => '',
                  'fragment_prefix' => '',
                  'apply_id_to_heading' => true,
                  'html_class' => 'c-anchor',
                  'aria_hidden' => false,
+                 'insert' => 'after',
+                 'symbol' => '#',
              ],
         ],
 
