84785cc4b372839b7741aa9e254c860e3b717f5b
Jay George <contact@jaygeorge.co.uk>
2025-10-24 11:31:52 +0100

    Change default font to sans-serif so the docs feel more doc-ish - finish

--- resources/js/site.js
+++ resources/js/site.js
@@ -6,7 +6,6 @@ import './collapsible-side-menu.js';
 import './anchors.js';
 import './cookies.js';
 import './color-scheme-preferences.js';
-import './font-preferences.js';
 import './external-links.js';
 import './dl.js';
 import './tables.js';
